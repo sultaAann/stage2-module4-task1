8063dab1d249a2f781b9edd73faf02d08dd453df
sultaAann <sultandjavgashev@gmail.com>
2023-06-25 22:00:21 +0500

    ok

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,12 +1,19 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    public static final ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private volatile static ThreadSafeSingleton instance;
 
     private ThreadSafeSingleton() {
     }
 
     public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
         return instance;
     }
 }
